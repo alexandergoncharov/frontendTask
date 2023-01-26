3cbd5fb9d005cdd929573b9ee997b8d578d6b898
Alexander Goncharov <a.goncharov@buzzguru.com>
2023-01-26 15:06:22 +0300

    Add login page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,5 +1,34 @@
+import { Button, Form, Input } from "antd";
+
 const Login = () => {
-    return <h1>Login</h1>;
+    return <Form
+        name="normal_login"
+        className="login-form"
+        initialValues={{ remember: true }}
+        onFinish={() => console.log("login")}
+    >
+        <Form.Item
+            name="username"
+            rules={[{ required: true, message: 'Please input your Username!' }]}
+        >
+            <Input placeholder="Username" />
+        </Form.Item>
+        <Form.Item
+            name="password"
+            rules={[{ required: true, message: 'Please input your Password!' }]}
+        >
+            <Input
+                type="password"
+                placeholder="Password"
+            />
+        </Form.Item>
+
+        <Form.Item>
+            <Button type="primary" htmlType="submit" className="login-form-button">
+                Log in
+            </Button>
+        </Form.Item>
+    </Form>;
 };
 
 export default Login;
