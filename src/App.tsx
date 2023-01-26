a406f230fa27b6cf9b5172df3b0098974d75202f
Alexander Goncharov <a.goncharov@buzzguru.com>
2023-01-26 12:04:30 +0300

    Add antd library

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { Layout } from "antd";
+import { Content, Header } from "antd/es/layout/layout";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
 import About from "./pages/About";
@@ -6,12 +8,19 @@ import Login from "./pages/Login";
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Routes>
-            <Route index element={<About />} />
-            <Route path="login" element={<Login />} />
-        </Routes>
-      </BrowserRouter>
+      <Layout>
+        <Content>
+          <div>
+            topbar
+          </div>
+          <BrowserRouter>
+            <Routes>
+              <Route index element={<About />} />
+              <Route path="login" element={<Login />} />
+            </Routes>
+          </BrowserRouter>
+        </Content>
+      </Layout>
     </div>
   );
 }
