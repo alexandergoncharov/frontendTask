c728c708d29bc6480b9b4cec4c719f7c066f4087
Alexander Goncharov <a.goncharov@buzzguru.com>
2023-01-26 11:57:19 +0300

    Add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
+import About from "./pages/About";
+import Login from "./pages/Login";
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
+        <Routes>
+            <Route index element={<About />} />
+            <Route path="login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
