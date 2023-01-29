0454416eb087eaeade16c9c69fdf5be813dd8c80
Alexander Goncharov <a.goncharov@buzzguru.com>
2023-01-29 19:36:03 +0300

    Show navbar buttons with tokens

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,13 +1,17 @@
 import { Space, Button } from 'antd';
+import { useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { UserContext } from '../context/UserContext';
 
 const NavBar = () => {
     let navigate = useNavigate();
+    const { userToken } = useContext(UserContext);
+
     return <Space size={"small"}>
         <Button onClick={() => navigate("/")}>About us</Button>
-        <Button onClick={() => navigate("/login")}>Sign in</Button>
-        <Button onClick={() => navigate("/profile")}>Profile</Button>
-        <Button>Sign out</Button>
+        {!userToken && <Button onClick={() => navigate("/login")}>Sign in</Button>}
+        {userToken && <Button onClick={() => navigate("/profile")}>Profile</Button>}
+        {userToken && <Button>Sign out</Button>}
     </Space>
 };
 
