febe8cc927a95f3b11531b78226ac2df239179f2
Alexander Goncharov <a.goncharov@buzzguru.com>
2023-01-29 18:02:39 +0300

    Fix login form

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,40 +1,57 @@
 import { Button, Form, Input, Typography } from "antd";
+import { LoginParams } from "../utils/types";
 
 const { Text } = Typography;
 
 const Login = () => {
-    return <Form
-        name="normal_login"
-        className="login-form"
-        initialValues={{ remember: true }}
-        onFinish={() => console.log("login")}
-    >
-        <Text>Email address</Text>
-        <Form.Item
-            name="email"
-            rules={[{ required: true, message: 'Please input your Username!' }]}
-        >
-            <Input placeholder="Enter email" />
-            <Text type="secondary">We'll never share your email with anyone else.</Text>
-        </Form.Item>
+    const onFinish = (values: LoginParams) => {
+        console.log(values);
+    };
 
-        <Text>Password</Text>
-        <Form.Item
-            name="password"
-            rules={[{ required: true, message: 'Please input your Password!' }]}
+    return (
+        <Form
+            name="normal_login"
+            className="login-form"
+            initialValues={{ remember: true }}
+            onFinish={onFinish}
         >
-            <Input
-                type="password"
-                placeholder="Password"
-            />
-        </Form.Item>
+            <Text>Email address</Text>
+            <Form.Item
+                name="email"
+                rules={[
+                    {
+                        type: "email",
+                        message: "The input is not valid E-mail!",
+                    },
+                    {
+                        required: true,
+                        message: "Please input your Email!",
+                    },
+                ]}
+            >
+                <Input placeholder="Enter email" />
+                <div>
+                    <Text type="secondary">
+                        We'll never share your email with anyone else.
+                    </Text>
+                </div>
+            </Form.Item>
 
-        <Form.Item>
-            <Button type="primary" htmlType="submit" className="login-form-button">
-                Submit
-            </Button>
-        </Form.Item>
-    </Form>;
+            <Text>Password</Text>
+            <Form.Item
+                name="password"
+                rules={[{ required: true, message: "Please input your Password!" }]}
+            >
+                <Input type="password" placeholder="Password" />
+            </Form.Item>
+
+            <Form.Item>
+                <Button type="primary" htmlType="submit" className="login-form-button">
+                    Submit
+                </Button>
+            </Form.Item>
+        </Form>
+    );
 };
 
 export default Login;
