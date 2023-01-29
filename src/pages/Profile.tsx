658a2834d3f438160dc26f9c833c3a10191b2e83
Alexander Goncharov <a.goncharov@buzzguru.com>
2023-01-29 22:00:12 +0300

    Add logic for getting name of the user

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,19 +1,30 @@
 import { Avatar, Button, Typography } from "antd";
+import { useEffect, useState } from "react";
 import userLogo from "../assets/userLogo.png";
+import { getProfileInfo } from "../service/Profile";
 
 const { Title } = Typography;
 
 const Profile = () => {
-  const name = "Alexey";
-  return (
-    <div className="profile">
-      <Avatar size={100} src={userLogo} />
-      <div className="welcome">
-        <Title className="welcome-title">Welcome, {name}!</Title>
-        <Button type="primary">Update</Button>
-      </div>
-    </div>
-  );
+    const [name, setName] = useState("Little story about company");
+
+    useEffect(() => {
+        const token = localStorage.getItem("userToken");
+        getProfileInfo(token!).then((profile: any) => {
+            const name = profile.fullname.split(" ")[0];
+            setName(name);
+        });
+    });
+
+    return (
+        <div className="profile">
+            <Avatar size={100} src={userLogo} />
+            <div className="welcome">
+                <Title className="welcome-title">Welcome, {name}!</Title>
+                <Button type="primary">Update</Button>
+            </div>
+        </div>
+    );
 };
 
 export default Profile;
